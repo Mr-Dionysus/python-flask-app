1a2a054e6be3f94e95046a7d28bdab5a22805505
Denis <dbreznikov@gmail.com>
2025-08-21 19:09:56 +0300

    feature: create a login page and add a validation

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import flash, redirect, render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -13,6 +14,14 @@ def index():
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/test")
-def test():
-    return "test 23123123"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect("/index")
+    return render_template("login.html", title="Sign In", form=form)
