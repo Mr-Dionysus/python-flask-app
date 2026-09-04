7b2386aa5a61c10d0b971385475027bb58d6c9e2
Denis <dbreznikov@gmail.com>
2026-09-04 13:09:18 +0300

    feat: add explore page, pagination, and post form

--- app/forms.py
+++ app/forms.py
@@ -61,3 +61,11 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField("Submit")
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField(
+        "Write here...",
+        validators=[DataRequired(), Length(min=1, max=300)],
+    )
+    submit = SubmitField("Submit")
